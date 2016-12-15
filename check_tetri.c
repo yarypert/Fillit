26c06a65c95f47f21af4fc9a15c2ac76eedea314
Josephine ROBIN <jorobin@e1r12p23.42.fr>
2016-12-15 17:15:12 +0100

    ajout des conditions de verification des tetriminos, notamment le fait qu'il ne faille pas depasser 26 (alphabet oblige)

--- check_tetri.c
+++ check_tetri.c
@@ -6,10 +6,17 @@
 /*   By: yarypert <yarypert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 16:54:01 by yarypert          #+#    #+#             */
-/*   Updated: 2016/12/15 16:57:53 by yarypert         ###   ########.fr       */
+/*   Updated: 2016/12/15 17:12:54 by jorobin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//si ya que des '.' et '#' et '\n'
+//si ya pas plus de 4 lignes
+//si ya 4 '#'
+//si ya 5 charac par ligne 
+//si ya 8 ou 6 "liens"
+//si ya des '/n' entre chaque piece
+//si ya plus de 26 tetriminos
 int		check_tetri(char *str)
 {
 	
