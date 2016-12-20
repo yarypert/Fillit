ce65deb2fca657baeb86edf3cb46a6be34896751
Josephine <jorobin@student.42.fr>
2016-12-20 19:09:39 +0100

    croustillon

--- debut_algo.c
+++ debut_algo.c
@@ -6,7 +6,7 @@
 /*   By: jorobin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 17:24:30 by jorobin           #+#    #+#             */
-/*   Updated: 2016/12/15 18:17:29 by yarypert         ###   ########.fr       */
+/*   Updated: 2016/12/16 19:25:45 by jorobin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,42 @@
 //jusqu'a ce qu'il n'y ai plus de tetriminos (fin du fichier ouvert)
 //- ma bite
 #include "libft.h"
+
+#define BUF_SIZE 20
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	int		ret; //pour le retour de read (nb d'octets lus ou -1 en cas d'erreur)
+	char	str[BUF_SIZE + 1];
+
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_putstr("open failed\n");
+			return (1);
+		}
+		while (ret = read(fd, &str, BUF_SIZE))//le fichier est stocke dans buf
+		{
+			str[ret] = '\0';
+			if (check_tetri(str) == 1)
+				return (1);
+			//ft_algo(str);
+			// put all fonctions here PD
+			ft_strclr(str);
+			ret++;
+		}
+		if (close(fd) == -1)
+		{
+			ft_putstr("close failed\n");
+			return (1);
+		}
+		return (0);
+	}
+	write(2, "error\n", 6);
+
+char	*ft_send_tetri(char *str, int *i)
+{
+}
