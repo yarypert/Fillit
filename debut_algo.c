7f534a0ba0ceadcc9a4a1985ffd9495a36c0ff2f
Yanis RYPERT <yarypert@e3r1p11.42.fr>
2016-12-20 15:28:43 +0100

    update

--- debut_algo.c
+++ debut_algo.c
@@ -6,7 +6,7 @@
 /*   By: jorobin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 17:24:30 by jorobin           #+#    #+#             */
-/*   Updated: 2016/12/15 18:17:29 by yarypert         ###   ########.fr       */
+/*   Updated: 2016/12/20 15:28:18 by yarypert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,27 @@
 //
 //Le tetriminos place sera le plus en haut et a gauche possible.
 //Il faut donc:
-//- une fonction qui check la position initiale du tetriminos qu'on testera dans
-//la fonction test de la meilleure position sauf pour le carre 
-//- une fonction qui change la position du tetriminos
-//- une fonction qui trouve la meilleure position du tetriminos (moins d'espace etc)
+//- Une fonction qui teste si les tetriminos sont tous valides 
 //- une fonction de placement du tetriminos au "carre" et qui passera au tetriminos suivant
 //jusqu'a ce qu'il n'y ai plus de tetriminos (fin du fichier ouvert)
 //- ma bite
+// kekosaure
+
+/*
+UPDATE
+ouvrir le fichier
+Lire le fichier
+stocker dans une chaine
+couper la chaine par pieces
+checker chaque pieces
+
+Si valide
+Placer le tetrimino
+checker si le placement est le plus opti
+repeter autant de fois qu'il y a de piece
+
+afficher grille
+FIN
+*/
+
 #include "libft.h"
