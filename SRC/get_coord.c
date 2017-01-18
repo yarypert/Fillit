941f26a7aeb50308a86d0065e16b751e8b10ad7e
Josephine ROBIN <jorobin@e2r5p20.42.fr>
2017-01-18 16:04:26 +0100

    changement de nom

--- SRC/get_coord.c
+++ SRC/get_coord.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zizi.c                                             :+:      :+:    :+:   */
+/*   get_coord.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jorobin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/01/18 15:02:02 by jorobin           #+#    #+#             */
-/*   Updated: 2017/01/18 16:00:33 by jorobin          ###   ########.fr       */
+/*   Created: 2017/01/18 16:02:30 by jorobin           #+#    #+#             */
+/*   Updated: 2017/01/18 16:04:04 by jorobin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-t_tetri		find_x_y(t_tetri	list)
+t_tetri		get_coord(t_tetri	list)
 {
 	char	tabxy[4];
 	int		i;
@@ -30,9 +30,9 @@ t_tetri		find_x_y(t_tetri	list)
 		{
 			if (list.tetri[i] == '#')
 			{
-					list.coord[j] = x;
-					list.coord[j + 1] = y;
-					x++;
+				list.coord[j] = x;
+				list.coord[j + 1] = y;
+				x++;
 			}
 			i++;
 		}
