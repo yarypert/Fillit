3be449eec9afcf7cf6c751e87cf08ec8a313beba
Yanis RYPERT <yarypert@e1r3p11.42.fr>
2017-02-04 15:52:44 +0100

    kek

--- Tab/SRC/add_to_list.c
+++ Tab/SRC/add_to_list.c
@@ -6,12 +6,13 @@
 /*   By: yarypert <yarypert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 18:15:21 by yarypert          #+#    #+#             */
-/*   Updated: 2017/02/02 07:49:06 by yarypert         ###   ########.fr       */
+/*   Updated: 2017/02/04 15:51:37 by yarypert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/fillit.h"
 
+/*
 t_tetri		*add_to_list(char *str, int piece,int letter)
 {
 	t_tetri *start;
@@ -23,4 +24,12 @@ t_tetri		*add_to_list(char *str, int piece,int letter)
 	ft_replace_char(start->tetri, '#', letter);
 	start->next = NULL;
 	return (start);
+}
+*/
+
+char	***add_to_tab(char *str, int piece, int letter)
+{
+	char	***tab;
+
+	return (tab);
 }
