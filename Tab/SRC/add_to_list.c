35449aec7460df38183824fe922a2a7c9adfe78b
Yanis RYPERT <yarypert@e1r4p10.42.fr>
2017-02-04 18:20:12 +0100

    commit ta mere ou je m'en fou

--- Tab/SRC/add_to_list.c
+++ Tab/SRC/add_to_list.c
@@ -6,30 +6,55 @@
 /*   By: yarypert <yarypert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 18:15:21 by yarypert          #+#    #+#             */
-/*   Updated: 2017/02/04 15:51:37 by yarypert         ###   ########.fr       */
+/*   Updated: 2017/02/04 18:11:06 by yarypert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/fillit.h"
-
+#include <stdio.h>
 /*
-t_tetri		*add_to_list(char *str, int piece,int letter)
+   t_tetri		*add_to_list(char *str, int piece,int letter)
+   {
+   t_tetri *start;
+
+   start = malloc(sizeof(*start));
+   start->index = piece + 1;
+   start->tetri = malloc(sizeof(char) * (ft_strlen(str) + 1));
+   ft_strcpy(start->tetri, str);
+   ft_replace_char(start->tetri, '#', letter);
+   start->next = NULL;
+   return (start);
+   }
+   */
+
+char	**str_to_tab(char *str)
 {
-	t_tetri *start;
+	char	**tetri;
+	int i;
 	
-	start = malloc(sizeof(*start));
-	start->index = piece + 1;
-	start->tetri = malloc(sizeof(char) * (ft_strlen(str) + 1));
-	ft_strcpy(start->tetri, str);
-	ft_replace_char(start->tetri, '#', letter);
-	start->next = NULL;
-	return (start);
+	i = 0;
+	tetri = ft_strsplit(str, '\n');
+	
+	while(i < 4)
+	{
+		tetri[i] = ft_strjoin(tetri[i],"\n");
+		i++;
+	}
+	return (tetri);
 }
-*/
 
-char	***add_to_tab(char *str, int piece, int letter)
+char	***tab_to_tabtab(char *str, int piece)
 {
-	char	***tab;
+	int		i;
+	char	**tetri;
+	char	***tab_tetri;
 
-	return (tab);
+	tetri = str_to_tab(str);
+	tab_tetri = malloc(sizeof(tetri) * piece);
+
+	while (i <= piece)
+	{
+		tab_tetri[i] = tetri;
+	}
+	return(tab_tetri);
 }
