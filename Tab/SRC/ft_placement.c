f5c8bf7116ac153fd03e1a72bd0b5ec0cd89fc7c
Josephine <jorobin@student.42.fr>
2017-02-18 19:41:04 +0100

    test d'algo

--- Tab/SRC/ft_placement.c
+++ Tab/SRC/ft_placement.c
@@ -12,9 +12,41 @@
 
 #include "../Includes/fillit.h"
 
+int		place_tetri(char **tab_tetri, char *grid, int n, t_var *var)
+{
+	int i;
+	int j;
+	char	**place_grid[n];
+
+	*place_grid[j] = grid;
+	i = 0;
+	while (place_grid[i])
+	{
+		while (place_grid[i] == '#' || place_grid[i] == 'x')
+		{
+			i++;
+		}
+		if (ft_check(grid, tab_tetri, i) == 0)
+			ft_place(grid, tab_tetri, i);//on place le tetri
+		if (ft_check(grid, tab_tetri, i) == 1)//pas bon placement
+			place_grid[i] = 'x';
+		if (ft_check(grid, tab_tetri, i) == -1)//la map des x a que des x
+		{
+			clr_place_grid(place_grid[j]);//clean la map_x
+			if (!(j - 1))//si ya pas de piece avant
+				return(1);//agrandir la map
+			j--; //map_x d'avant
+			return(-1);//passer a la piece d'avant
+		}
+	}
+	return(0);
+}
+
+
+
 void	ft_placement(int nbpiece, char ***tab_tetri)
 {
-	t_var	var;
+	t_var	*var;
 	char	*grid = NULL;
 	int		i;
 
@@ -23,19 +55,12 @@ void	ft_placement(int nbpiece, char ***tab_tetri)
 	while (tab_tetri[i] != NULL)
 	{
 		move_tetri(tab_tetri[i]);
-		//place_tetri(var, **tab_tetri[i], grid);
-		while (var.flag != 0)//tant qu'on a pas bien place une piece
+		while (place_tetri((char**)tab_tetri[i], grid, i, var) != 0)//tant qu'on a pas bien place une piece
 		{
-			if (var.flag == 1)//plus de possibilite d'aller une piece en arriere
-				//>>>>> agrandir la map de 1
-				grid_1_up(i);
-			if (var.flag == -1)
-				//revenir a la piece precedente si pas d'autre placement
+			if (place_tetri((char**)tab_tetri[i], grid, i, var) == 1)//plus de possibilite d'aller une piece en arriere
+				grid_1_up(i);//agrandir la map de 1
+			if (place_tetri((char**)tab_tetri[i], grid, i, var) == -1)
 				i--;
-			if (var.flag == 2)
-				//place_tetri(var, **tab_tetri[i],grid);
-				ft_void();
-				//tester un autre placement
 		}
 		i++;
 	}
