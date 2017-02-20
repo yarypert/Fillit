808a7473d968fb49367ab501e76f03a731e208a0
Josephine ROBIN <jorobin@e1r8p21.42.fr>
2017-02-20 11:20:00 +0100

    ft_placement correction 11h19, revision des check a faire et tests

--- Tab/SRC/ft_placement.c
+++ Tab/SRC/ft_placement.c
@@ -6,54 +6,33 @@
 /*   By: jorobin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 14:18:47 by jorobin           #+#    #+#             */
-/*   Updated: 2017/02/19 13:18:49 by jorobin          ###   ########.fr       */
+/*   Updated: 2017/02/20 11:18:57 by jorobin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/fillit.h"
 
-char	*clr_place_grid(char *grid)
+char	**clr_place_grid(char **grid)
 {
 	int		i;
+	int		j;
 
 	i = 0;
-	while (grid[i])
+	while (grid[j][i])
 	{
-		while (grid[i] != '\n')
+		while (grid[j][i] != '\n')
 		{
-			grid[i] = '.';
+			grid[j][i] = '.';
+			i++;
 		}
-		i++;
+		grid[j][i] = '.';
+		j++;
 	}
 	return(grid);
 }
 
-char	*ft_place(char *grid, char **tab_tetri, int place)
+char	**ft_place(char **grid, char **tab_tetri, int place)
 {
-	int		i;
-	int		j;
-	int		k;
-/*
-	i = place;
-	j = 0;
-	k = 0;
-	while (grid[i] != '\0')
-	{
-		while (k != 4)
-		{
-			if (grid[i] == '.')
-			{
-				while (*tab_tetri[j] == '.')
-					j++;
-				grid[i] = *tab_tetri[j];
-				i++;
-				j++;
-				k++;
-			}
-			if (grid[i] == '\n')
-				i++;
-		}
-	}*/
 	return(grid);
 }
 
@@ -75,92 +54,76 @@ char	find_letter(char **tetri)
 	return(letter);
 }
 
-int		ft_check(char *grid, char **tetri, int place)
+int		ft_check(char **grid, char **tetri, int place)
 {//fonction check si la place "int place" est valide
 //c'est a dire si on peut placer le tetri **tab_tetri a cet endroit
 //si on ne peut pas on renvoit 1
 //si on peut on renvoit 0
-
-	int i;
-	int j;
-	int k;
-	char let;
-
-	i = place;
-	j = 0;
-	k = 0;
-	let = find_letter(tetri);
-	while (grid[i] != '\0')
-	{
-		while (k < 4 || tetri[j][i])
-		{
-			if (grid[i] == '.')
-			{
-				while (tetri[j][i] == '.')
-					j++;
-				grid[i] = tetri[j][i];
-				if (k != 0)//si une partie de piece est deja placee
-				{
-					//regarder si 
-					if (grid[i - 1] != let || grid[i + 1] != let || grid[i + 5] != let)
-						return(1);
-				}
-				k++;
-				i++;
-			}
-			if (grid[i] == '\n')
-			{
-				if (tetri[j][i + 1] != '.' || tetri[j][i + 1] != '\n')
-					return(1);
-				i++;
-			}
-		}
 		return(0);
-	}
-	return(1);
 }
 
-int		place_tetri(char **tetri, char **map, int n)//n est le nb de piece
+int		find_x(char *str)//pour trouver le decalage
 {
-	int		i;
-	int		j;
-	char	**map_x;
+	int x;
+	int i;
 
-	map_x = (char**)malloc(sizeof(char*) * n + 1);
+	x = -1;
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] > 'A' && str[i] < 'Z') && (x = -1))
+			x = str[i];
+		i++;
+	}
+	return(x);
+}
+
+int		check_fonction(char **tetri, char **map, int n)//n est le nb de piece
+{
+	int		i;//compteur
+	int		j;//compteur
+	int		k;
+	char	***map_x;//la map des places invalides
+
 	i = 0;
 	j = 0;
-	map_x[j] = *map;
+	k = 0;
 
-	while (map_x[j])
+	map_x = (char***)malloc(sizeof(char**) * n + 1);
+
+	map_x[k] = map;
+
+	while (map_x[k][j][i])//un k par piece, j par ligne, i par lettre (cimer le char*** ;))
 	{
-		while ((map[j][i] > 'A' && map[j][i] < 'Z') || map_x[j][i] == 'x')//ce while detecte et remplace les cases invalide par des X
+		while (map_x[k][j][i] != '\n')
 		{
-			map_x[j][i] = 'x';
-			i++;
+			while ((map[j][i] > 'A' && map[j][i] < 'Z') || map_x[k][j][i] == 'x')//ce while detecte et remplace les cases invalide par des X
+			{
+				map_x[k][j][i] = 'x';
+				i++;
+			}
+			if (ft_check(map, tetri, i) == 0)
+			{
+				map = ft_place(map, tetri, i);//on place le tetri
+				k++;
+				return(0);
+			}
+			if (ft_check(map, tetri, i) == 1)//pas bon placement
+				map_x[k][j][i] = 'x';
+			if (ft_check(map, tetri, i) == -1)//la map des x a que des x
+			{
+				clr_place_grid(map_x[k]);//clean la map_x
+				if (!(j - 1))//si ya pas de piece avant
+					return(1);//agrandir la map
+				k--; //map_x d'avant
+				return(-1);//passer a la piece d'avant
+			}
 		}
-		if (ft_check(*map, tetri, i) == 0)
-		{
-			*map = ft_place(*map, tetri, i);//on place le tetri
-			j++;
-			return(0);
-		}
-		if (ft_check(*map, tetri, i) == 1)//pas bon placement
-			map_x[j][i] = 'x';
-		if (ft_check(*map, tetri, i) == -1)//la map des x a que des x
-		{
-			clr_place_grid(map_x[j]);//clean la map_x
-			if (!(j - 1))//si ya pas de piece avant
-				return(1);//agrandir la map
-			j--; //map_x d'avant
-			return(-1);//passer a la piece d'avant
-		}
-		if (map[j][i] == '\n')
-			i++;
+		j++;
+		i = 0;
 	}
 	return(1);
 }
-
-
 
 void	ft_placement(int nbpiece, char ***tab_tetri)
 {
@@ -172,11 +135,11 @@ void	ft_placement(int nbpiece, char ***tab_tetri)
 	while (tab_tetri[i] != NULL)
 	{
 		move_tetri(tab_tetri[i]);
-		while (place_tetri(tab_tetri[i], grid, nbpiece) != 0)//tant qu'on a pas bien place une piece
+		while (check_fonction(tab_tetri[i], grid, nbpiece) != 0)//tant qu'on a pas bien place une piece
 		{
-			if (place_tetri(tab_tetri[i], grid, nbpiece) == 1)//plus de possibilite d'aller une piece en arriere
+			if (check_fonction(tab_tetri[i], grid, nbpiece) == 1)//plus de possibilite d'aller une piece en arriere
 				grid_1_up(i);//agrandir la map de 1
-			if (place_tetri(tab_tetri[i], grid, nbpiece) == -1)
+			if (check_fonction(tab_tetri[i], grid, nbpiece) == -1)
 				i--;
 		}
 		i++;
