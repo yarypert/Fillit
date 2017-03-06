10899185f785b9f051a4d19233099b665181496d
Sholyu <yarypert@student.42.fr>
2017-03-06 16:30:58 +0100

    clean

--- test/SRC/count_piece.c
+++
@@ -1,9 +0,0 @@
-#include "../Includes/fillit.h"
-
-int		count_piece(int len)
-{
-	int i;
-
-	i = (len / 21) + 1;
-	return (i);
-}
--- test/libft/ft_void.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void	ft_void(void)
-{
-	return;
-}
