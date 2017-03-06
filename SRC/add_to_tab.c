fcaad7e0183bc5bed559c4de924f41d0580585c9
kainomel <jorobin@student.42.fr>
2017-03-06 23:32:21 +0100

    add to tab raccourci pour la norme

--- SRC/add_to_tab.c
+++ SRC/add_to_tab.c
@@ -15,27 +15,37 @@
 char	**str_to_tab(char *str)
 {
 	char	**tab;
-	int i;
-	int j;
-	int start;
-	int len;
+	int		i;
+	int		j;
+	int		len;
 
 	j = 0;
 	i = 0;
 	len = ft_strlen(str);
 	if (str[len - 2] != '#' && str[len - 2] != '.')
 		ft_error("error");
-	tab = (char **)malloc(sizeof(char *) * (count_piece(len)) + 1);
+	if(!(tab = (char **)malloc(sizeof(char *) * (count_piece(len)) + 1)))
+		return(NULL);
 	while (j < count_piece(len))
 	{
-		tab[j] = (char *)malloc(sizeof(char) * 20);
+		if(!(tab[j] = (char *)malloc(sizeof(char) * 20)))
+			return(NULL);
 		j++;
 	}
 	tab[j] = NULL;
-	start = 0;
-
 	if (len > 545 || len == 0)
 		ft_error("error");
+	tab = str_to_tab_2(len, str, tab);
+	return(tab);
+}
+
+char	**str_to_tab_2(int len, char *str, char **tab)
+{
+	int		i;
+	int		start;
+
+	start = 0;
+	i = 0;
 	while (start < len)
 	{
 		tab[i] = ft_strsub(str,start,20);
